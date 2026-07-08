02f2c1a0f8f5494411bdd37a6543092b7a69192d
Skoulskiy <ogurethcs@gmail.com>
2026-07-08 14:49:54 +0300

    fix commit

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,7 @@
-export const Sum = (props) => {
-  const { a, b } = props;
-
+export const Sum = ({ a = 0, b = 0}) => {
   return (
-    <p>Sum of {a || 0} and {b || 0} is {a || 0 + b || 0}</p>
+    <p>
+      {`Sum of ${a} and ${b} is ${a + b}`}
+    </p>
   );
 };
